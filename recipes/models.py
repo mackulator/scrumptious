653ab3a1c09e3e9430c194373b503f0a29293f04
Mackenzie Hill <hillmack92@gmail.com>
2022-10-16 11:38:59 -0300

    working create, view index routes

---
+++ recipes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+  title = models.CharField(max_length=200)
+  picture = models.URLField(null=True, max_length=205)
+  description = models.TextField()
+  created_on = models.DateTimeField(auto_now_add=True)
