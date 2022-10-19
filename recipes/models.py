da20e6429b24f9ded0c3f97fe5cf1a2857a9f571
Mackenzie Hill <hillmack92@gmail.com>
2022-10-19 09:37:22 -0300

    sign up

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
   title = models.CharField(max_length=200)
-  picture = models.URLField(null=True, max_length=205)
+  # author = models.ForeignKey(
+  #   related_name='recipes',
+  #   on_delete=models.CASCADE,
+  #   null=True,
+  # )
+  picture = models.URLField(null=True, blank=True)
   description = models.TextField()
-  created_on = models.DateTimeField(auto_now_add=True)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
