e93c6112229292be2e08cdd09b25664aa409449e
Mackenzie Hill <hillmack92@gmail.com>
2022-10-19 10:19:51 -0300

    loginform

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from accounts.views import signup
+from accounts.views import signup, user_login
 
 
 urlpatterns = [
-  path("signup/", signup, name="signup")
+  path("signup/", signup, name="signup"),
+  path("login/", user_login, name="user_login"),
+
 ]
