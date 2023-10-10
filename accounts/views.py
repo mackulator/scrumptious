8c1bea050fb56fdf7b86180fbec8629a64beded7
Mack Hill <hillmack92@gmail.com>
2023-10-10 10:34:49 -0500

    pushing wap

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from accounts.forms import SignUpForm, LoginForm
 
 
 # Create your views here.
+
+def user_logout(request):
+  logout(request)
+  return redirect("recipe_list")
 
 def user_login(request):
   if request.method == "POST":
@@ -19,14 +23,14 @@ def user_login(request):
         password=password,
       )
       if user is not None:
-        login(request,user)
+        login(request, user)
         return redirect("recipe_list")
-    else:
-      form = LoginForm()
-    context = {
-      "form": form,
-    }
-    return render(request, "accounts/login.html", context)
+  else:
+    form = LoginForm()
+  context = {
+    "form": form,
+  }
+  return render(request, "accounts/login.html", context)
 
 def signup(request):
   if request.method == "POST":
@@ -39,18 +43,18 @@ def signup(request):
       last_name = form.cleaned_data['last_name']
 
       if password == password_confirmation:
-          # create new user with those values
-          # and save to variable
-          user = User.objects.create_user(
-            username,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-          )
+        # create new user with those values
+        # and save to variable
+        user = User.objects.create_user(
+          username,
+          password=password,
+          first_name=first_name,
+          last_name=last_name,
+        )
 
-          # login user with user just creatd
-          login(request, user)
-          return redirect("recipe_list")
+        # login user with user just creatd
+        login(request, user)
+        return redirect("recipe_list")
       else:
         form.add_error("password", "Passwords do not match")
   else:
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 from .forms import RecipeForm
 
@@ -8,7 +9,7 @@ from .forms import RecipeForm
 def show_recipe(request, id):
   recipe = get_object_or_404(Recipe, id=id)
   context = {
-    "recipe_list": recipe,
+    "recipe_object": recipe,
   }
   return render(request, "recipes/detail.html", context)
 
@@ -19,15 +20,18 @@ def recipe_list(request):
   }
   return render(request, "recipes/list.html", context)
 
+@login_required
 def create_recipe(request):
   if request.method == "POST":
     form = RecipeForm(request.POST)
     if form.is_valid():
-      form.save()
+      recipe = form.save(False)
+      recipe.author = request.user
+      recipe.save()
+
       return redirect("recipe_list")
   else:
     form = RecipeForm()
-
   context = {
     "form": form
   }
@@ -47,6 +51,20 @@ def edit_recipe(request, id):
     "recipe_form": form,
   }
   return render(request, "recipes/edit.html", context)
+
+@login_required
+def my_recipe_list(request):
+  recipes = Recipe.objects.filter(author=request.user)
+  context = {
+    "recipe_list": recipes,
+  }
+  return render(
+    request,
+    "recipes/list.html",
+    context
+    )
+
+
 
 # def sign_up(request):
 #   if request.method == "POST":
